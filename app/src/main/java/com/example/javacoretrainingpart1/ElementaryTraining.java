13711f8a755d2d62dec56d5e518e6c7ee6b5eeb6
Faimi the Legend Zufarov <faimi14102001@gmail.com>
2024-02-07 13:17:29 +0400

    Initial commit

---
+++ app/src/main/java/com/example/javacoretrainingpart1/ElementaryTraining.java
@@ -0,0 +1,113 @@
+package com.example.javacoretrainingpart1;
+
+/**
+ * Набор тренингов по работе с примитивными типами java.
+ * <p>
+ * Задания определены в комментариях методов.
+ * <p>
+ * Проверка может быть осуществлена запуском тестов.
+ * <p>
+ * Доступна проверка тестированием @see ElementaryTrainingTest.
+ */
+public class ElementaryTraining {
+
+    /**
+     * Метод должен возвращать среднее значение
+     * для введенных параметров
+     *
+     * @param firstValue  первый элемент
+     * @param secondValue второй элемент
+     * @return среднее значение для введенных чисел
+     */
+    public double averageValue(int firstValue, int secondValue) {
+        return (firstValue + secondValue) / 2.0;
+    }
+
+    /**
+     * Пользователь вводит три числа.
+     * Произвести манипуляции и вернуть сумму новых чисел
+     *
+     * @param firstValue  увеличить в два раза
+     * @param secondValue уменьшить на три
+     * @param thirdValue  возвести в квадрат
+     * @return сумма новых трех чисел
+     */
+    public double complicatedAmount(int firstValue, int secondValue, int thirdValue) {
+        int result = firstValue * 2 + (secondValue - 3) + thirdValue * thirdValue;
+        return (double) result;
+    }
+
+    /**
+     * Метод должен поменять значение в соответствии с условием.
+     * Если значение больше 3, то увеличить
+     * на 10, иначе уменьшить на 10.
+     *
+     * @param value число для изменения
+     * @return новое значение
+     */
+    public int changeValue(int value) {
+        if (value > 3) {
+            return value+10;
+        }
+        else {
+            return value-10;
+        }
+    }
+
+    /**
+     * Метод должен менять местами первую
+     * и последнюю цифру числа.
+     * Обрабатывать максимум пятизначное число.
+     * Если число < 10, вернуть
+     * то же число
+     *
+     * @param value число для перестановки
+     * @return новое число
+     */
+    public int swapNumbers(int value) {
+        String strNumberValue = String.valueOf(value);
+        if (strNumberValue.length() > 5) {
+            System.out.println("Обработаю максимум пятизначное число. Больше не буду :)");
+            return 0;
+        }
+        else if (value < 10) {
+            return value;
+        }
+        else {
+            String result = strNumberValue.charAt(strNumberValue.length()-1)
+                            + strNumberValue.substring(1, strNumberValue.length()-1)
+                            + strNumberValue.charAt(0);
+            return Integer.parseInt(result);
+        }
+    }
+
+    /**
+     * Изменить значение четных цифр числа на ноль.
+     * Счет начинать с единицы.
+     * Обрабатывать максимум пятизначное число.
+     * Если число < 10 вернуть
+     * то же число.
+     *
+     * @param value число для изменения
+     * @return новое число
+     */
+    public int zeroEvenNumber(int value) {
+        String strNumberValue = String.valueOf(value);
+        if (strNumberValue.length() > 5) {
+            System.out.println("Обработаю максимум пятизначное число. Больше не буду :)");
+            return 0;
+        }
+        else if (value < 10) {
+            return value;
+        }
+        else {
+            for (int i = 0; i < strNumberValue.length(); i++){
+                if ((int) strNumberValue.charAt(i) % 2 == 0) {
+                    strNumberValue = strNumberValue
+                            .replace(strNumberValue.charAt(i), '0');
+                }
+            }
+            return Integer.parseInt(strNumberValue);
+        }
+    }
+}
