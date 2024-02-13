a7d51ded637d7fa1ec9ad35589f04059ca3f8997
Faimi the Legend Zufarov <faimi14102001@gmail.com>
2024-02-13 20:54:18 +0400

    Java pt.2

---
+++ app/src/main/java/com/example/javacoretrainingpart1/classes/task7/Product.java
@@ -0,0 +1,13 @@
+package com.example.javacoretrainingpart1.classes.task7;
+
+public class Product {
+
+    String name = "";
+    int quantity = 0;
+
+    Product(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+}
