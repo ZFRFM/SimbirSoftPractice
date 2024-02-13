27bdd76b3118d6ed8f1e50b380e84931e27eab27
Faimi the Legend Zufarov <faimi14102001@gmail.com>
2024-02-13 23:25:20 +0400

    Доп. задание с лямбдой

---
+++ app/src/main/java/com/example/javacoretrainingpart1/LambdaClass.java
@@ -0,0 +1,35 @@
+package com.example.javacoretrainingpart1;
+
+
+/*
+
+Написать простое лямбда-выражение в переменной myClosure,
+лямбда-выражение должно выводить в консоль фразу "I love Java".
+Вызвать это лямбда-выражение. Далее написать функцию, которая будет
+запускать заданное лямбда-выражение заданное количество раз.
+Объявить функцию так: public void repeatTask (int times, Runnable task).
+Функция должна запускать times раз лямбда-выражение task .
+Используйте эту функцию для печати "I love Java" 10 раз.
+
+*/
+
+public class LambdaClass {
+    public static void main(String[] args) {
+        Javable myClosure = System.out::println;
+        myClosure.print("I love Java");
+        Runnable task = () -> {
+            myClosure.print("I love Java");
+        };
+        repeatTask(10, task);
+    }
+
+    public static void repeatTask (int times, Runnable task) {
+        for (int i = 0; i < times; i++) {
+            task.run();
+        }
+    }
+}
+
+interface Javable {
+    void print(String s);
+}
