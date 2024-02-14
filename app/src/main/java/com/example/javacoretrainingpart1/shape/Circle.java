3ebd27e5b32d238ed96196c80ee83234a1a02d1f
Faimi the Legend Zufarov <faimi14102001@gmail.com>
2024-02-14 12:59:54 +0400

    4 и 5 задачи доп. задания Java pt.2. На этом с Java покончено

---
+++ app/src/main/java/com/example/javacoretrainingpart1/shape/Circle.java
@@ -0,0 +1,14 @@
+package com.example.javacoretrainingpart1.shape;
+
+public class Circle implements Shape {
+
+    double diameter = 0.0;
+    double perimeter = 0;
+    double area = 0;
+
+    Circle(double diameter) {
+        this.diameter = diameter;
+        perimeter = diameter * 3.14;
+        area = 3.14 * (diameter/2) * (diameter/2);
+    }
+}
