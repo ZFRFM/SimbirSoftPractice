6b54dc426e621599b1ae4955bcfdbc381078effe
Faimi the Legend Zufarov <faimi14102001@gmail.com>
2024-02-14 12:59:54 +0400

    4 и 5 задачи доп. задания Java pt.2. На этом с Java покончено

---
+++ app/src/main/java/com/example/javacoretrainingpart1/shape/Square.java
@@ -0,0 +1,14 @@
+package com.example.javacoretrainingpart1.shape;
+
+public class Square implements Shape {
+
+    int lengthOfSide = 0;
+    double perimeter = 0.0;
+    double area = 0.0;
+
+    Square(int lengthOfSide) {
+        this.lengthOfSide = lengthOfSide;
+        perimeter = lengthOfSide * 4.0;
+        area = (double) (lengthOfSide * lengthOfSide);
+    }
+}
