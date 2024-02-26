8587688c6f561b9997c89401ca2238cab5cd1a06
Faimi the Legend Zufarov <faimi14102001@gmail.com>
2024-02-26 19:44:48 +0400

    Added Kotlin Pt.2

---
+++ app/src/main/java/ru/faimizufarov/simbirtraining/kotlin2/Login.kt
@@ -0,0 +1,5 @@
+package ru.faimizufarov.simbirtraining.kotlin2
+
+class Login(user: User): Action() {
+
+}
---
+++ app/src/main/java/ru/faimizufarov/simbirtraining/kotlin2/User.kt
@@ -0,0 +1,25 @@
+package ru.faimizufarov.simbirtraining.kotlin2
+
+import android.util.Log
+import java.util.Calendar
+
+data class User(
+    val id: Int,
+    val name: String,
+    val age: Int,
+    val type: Type
+) {
+    val startTime by lazy {
+        Calendar.getInstance()
+    }
+}
+
+fun User.isAdult(): Boolean {
+    if (this.age > 18) {
+        Log.d(TAG, this.toString())
+        return true
+    }
+    else {
+        throw Exception("This man is 18 years old or younger")
+    }
+}
