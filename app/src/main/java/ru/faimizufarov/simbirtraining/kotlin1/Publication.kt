ce2c33579c3a2fd5a85d54b55e818e723318f87f
Faimi the Legend Zufarov <faimi14102001@gmail.com>
2024-03-02 10:07:11 +0400

    returned kotlin1 files

---
+++ app/src/main/java/ru/faimizufarov/simbirtraining/kotlin1/Publication.kt
@@ -0,0 +1,15 @@
+package ru.faimizufarov.simbirtraining.kotlin1
+
+import java.util.Currency
+
+interface Publication {
+    val price: Int
+    val wordCount: Int
+
+    fun getType(): String
+}
+
+fun Publication.returnInfoAboutPublication(): String {
+    val euro = Currency.getInstance("EUR").symbol
+    return "${this.getType()}; ${this.price} $euro; ${this.wordCount}"
+}
