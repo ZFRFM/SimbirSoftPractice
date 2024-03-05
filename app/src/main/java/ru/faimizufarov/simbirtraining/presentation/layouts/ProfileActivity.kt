a188d67188df3aa9ebc68c093ce3e3d1e7e44ef3
Faimi the Legend Zufarov <faimi14102001@gmail.com>
2024-03-05 16:19:21 +0400

    Analyzed code and fixed recomendations

--- app/src/main/java/ru/faimizufarov/simbirtraining/presentation/layouts/ProfileActivity.kt
+++ app/src/main/java/ru/faimizufarov/simbirtraining/presentation/layouts/ProfileActivity.kt
@@ -1,13 +1,15 @@
 package ru.faimizufarov.simbirtraining.presentation.layouts
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import ru.faimizufarov.simbirtraining.R
 import ru.faimizufarov.simbirtraining.databinding.ActivityProfileBinding
 
+@Suppress("ktlint:standard:no-empty-first-line-in-class-body")
 class ProfileActivity : AppCompatActivity() {
 
-    lateinit var viewBinding: ActivityProfileBinding
+    private lateinit var viewBinding: ActivityProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityProfileBinding.inflate(layoutInflater)
