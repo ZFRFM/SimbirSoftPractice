ec65abf69b94243d82afa07d4f92131afab1a58c
Faimi the Legend Zufarov <faimi14102001@gmail.com>
2024-03-30 23:02:56 +0400

    Added Parcelize

--- app/src/main/java/ru/faimizufarov/simbirtraining/java/data/HelpCategoryEnum.kt
+++ app/src/main/java/ru/faimizufarov/simbirtraining/java/data/HelpCategoryEnum.kt
@@ -1,9 +1,10 @@
 package ru.faimizufarov.simbirtraining.java.data
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import ru.faimizufarov.simbirtraining.R
 
+@Parcelize
 enum class HelpCategoryEnum(
     val id: Int,
     val imageView: Int,
@@ -14,34 +15,4 @@ enum class HelpCategoryEnum(
     ELDERLY(2, imageView = R.drawable.elderly_category, nameCategory = R.string.elderly),
     ANIMALS(3, imageView = R.drawable.animals_category, nameCategory = R.string.animals),
     EVENTS(4, imageView = R.drawable.events_category, nameCategory = R.string.events),
-    ;
-
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readInt(),
-        parcel.readInt(),
-    ) {
-    }
-
-    override fun writeToParcel(
-        parcel: Parcel,
-        flags: Int,
-    ) {
-        parcel.writeInt(imageView)
-        parcel.writeInt(nameCategory)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<HelpCategoryEnum> {
-        override fun createFromParcel(parcel: Parcel): HelpCategoryEnum {
-            return entries[parcel.readInt()]
-        }
-
-        override fun newArray(size: Int): Array<HelpCategoryEnum?> {
-            return arrayOfNulls(size)
-        }
-    }
 }
