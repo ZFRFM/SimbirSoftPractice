cde7f953ff7a36d772af6fafeb0573288320b600
arantier <shcherbakov_dmitry@bk.ru>
2024-05-19 17:15:27 +0300

    Removed "checked" flag from HelpCategoryEnumResponse and it's asset

--- app/src/main/java/ru/faimizufarov/simbirtraining/java/data/HelpCategoryEnumResponse.kt
+++ app/src/main/java/ru/faimizufarov/simbirtraining/java/data/HelpCategoryEnumResponse.kt
@@ -3,7 +3,4 @@ package ru.faimizufarov.simbirtraining.java.data
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class HelpCategoryEnumResponse(
-    val id: Int,
-    val checked: Boolean,
-)
+data class HelpCategoryEnumResponse(val id: Int)
