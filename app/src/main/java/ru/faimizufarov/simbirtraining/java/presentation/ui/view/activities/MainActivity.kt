19455af5069c9c4dd42bb05f2b9aed42501ffabd
Faimi the Legend Zufarov <faimi14102001@gmail.com>
2024-06-20 19:16:59 +0400

    Rewrote MainActivity in MVVM pattern

--- app/src/main/java/ru/faimizufarov/simbirtraining/java/presentation/ui/view/activities/MainActivity.kt
+++ app/src/main/java/ru/faimizufarov/simbirtraining/java/presentation/ui/view/activities/MainActivity.kt
@@ -1,22 +1,22 @@
 package ru.faimizufarov.simbirtraining.java.presentation.ui.view.activities
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.launch
 import ru.faimizufarov.simbirtraining.R
 import ru.faimizufarov.simbirtraining.databinding.ActivityMainBinding
-import ru.faimizufarov.simbirtraining.java.presentation.ui.view.fragments.BadgeCounter
 import ru.faimizufarov.simbirtraining.java.presentation.ui.view.fragments.CategoriesFragment
 import ru.faimizufarov.simbirtraining.java.presentation.ui.view.fragments.NewsFragment
 import ru.faimizufarov.simbirtraining.java.presentation.ui.view.fragments.ProfileFragment
 import ru.faimizufarov.simbirtraining.java.presentation.ui.view.fragments.SearchFragment
+import ru.faimizufarov.simbirtraining.java.presentation.ui.viewmodel.MainViewModel
 
 @Suppress("ktlint:standard:no-empty-first-line-in-class-body")
 class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,10 +35,8 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
-        lifecycleScope.launch {
-            BadgeCounter.badgeCounter.collect {
-                updateBadgeCount(it)
-            }
+        mainViewModel.badgeCount.observe(this) {
+            updateBadgeCount(it)
         }
     }
 
