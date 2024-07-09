b3f553aa56d4a1f5f6028c3a1f51b2d3785f4861
Faimi the Legend Zufarov <faimi14102001@gmail.com>
2024-07-09 16:48:57 +0400

    Deleted dependencies in App object

--- app/src/main/java/ru/faimizufarov/simbirtraining/java/App.kt
+++ app/src/main/java/ru/faimizufarov/simbirtraining/java/App.kt
@@ -1,18 +1,11 @@
 package ru.faimizufarov.simbirtraining.java
 
 import android.app.Application
-import ru.faimizufarov.simbirtraining.java.data.repository.CategoryRepositoryImpl
-import ru.faimizufarov.simbirtraining.java.data.repository.NewsRepositoryImpl
 import ru.faimizufarov.simbirtraining.java.di.AppComponent
 import ru.faimizufarov.simbirtraining.java.di.AppModule
 import ru.faimizufarov.simbirtraining.java.di.DaggerAppComponent
-import ru.faimizufarov.simbirtraining.java.presentation.ui.holders.GlobalNewsFilter
 
 class App : Application() {
-    val newsRepositoryImpl by lazy { NewsRepositoryImpl(applicationContext) }
-    val categoriesRepository by lazy { CategoryRepositoryImpl(applicationContext) }
-    val newsFilters by lazy { GlobalNewsFilter() }
-
     lateinit var appComponent: AppComponent
 
     override fun onCreate() {
